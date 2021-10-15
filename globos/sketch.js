f111ecabead2deede0a908ddbb6cffedc799eb90
HaCkEru2839ud <92610150+HaCkEru2839ud@users.noreply.github.com>
2021-10-15 18:08:19 -0500

    Add files via upload

---
+++ globos/sketch.js
@@ -0,0 +1,92 @@
+var bow , arrow,  background;
+var bowImage, arrowImage, green_balloonImage, red_balloonImage, pink_balloonImage ,blue_balloonImage, backgroundImage;
+
+function preload(){
+  
+  backgroundImage = loadImage("background0.png");
+  arrowImage = loadImage("arrow0.png");
+  bowImage = loadImage("bow0.png");
+  red_balloonImage = loadImage("red_balloon0.png");
+  
+}
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  
+  //crea el fondo
+  scene = createSprite(0,0,400,400);
+  scene.addImage(backgroundImage);
+  scene.scale = 2.5
+  
+  //crea el arco para disparar las flechas
+  bow = createSprite(380,220,20,50);
+  bow.addImage(bowImage); 
+  bow.scale = 1;
+  
+}
+
+function draw() {
+ background(0);
+  //fondo en movimiento
+    scene.velocityX = -3 
+
+    if (scene.x < 0){
+      scene.x = scene.width/2;
+    }
+  
+  //arco en movimiento
+  bow.y = World.mouseY
+  
+   //suelta la flecha cuando se presione la tecla de barra espaciadora
+  if (keyDown("space")) {
+    createArrow();
+    
+  }
+  
+  //crea globos de forma continua 
+  var select_balloon = Math.round(random(1,1));
+  
+  if (World.frameCount % 100 == 0) {
+    if (select_balloon == 1) {
+      redBalloon();
+    }
+  }
+  
+  drawSprites();
+}
+
+
+//crea las flechas para el arco
+ function createArrow() {
+  var arrow= createSprite(100, 100, 60, 10);
+  arrow.addImage(arrowImage);
+  arrow.x = 360;
+  arrow.y=bow.y;
+  arrow.velocityX = -4;
+  arrow.lifetime = 100;
+  arrow.scale = 0.3;
+}
+
+
+function redBalloon() {
+  var red = createSprite(0,Math.round(random(20, 370)), 10, 10);
+  red.addImage(red_balloonImage);
+  red.velocityX = 3;
+  red.lifetime = 150;
+  red.scale = 0.1;
+
+}
+
+function blueBalloon() {
+  //escribe el código para aparecer los globos azules 
+}
+
+function greenBalloon() {
+  //escribe el código para aparecer los globos verdes
+}
+
+function pinkBalloon() {
+  //escribe el código para aparecer los globos rosas
+}
